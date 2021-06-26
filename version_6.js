03079bd187b9d5da7b74c3e03fe2ba0141be1450
LeomharT <13055988395@163.com>
2021-06-26 13:41:50 +0800

    version_6

---
+++ version_6.js
@@ -0,0 +1,77 @@
+const FS = require("fs");
+const PATH = require('path');
+const CHEEIRO = require('cheerio');
+
+const getFileList = (fs, folder, fileList, fileSeparator) => {
+
+    if (!(fs.statSync(folder).isDirectory())) { return; }
+
+    try {
+        let pathArray = fs.readdirSync(folder);
+
+        return pathArray.reduce((prev, curr) => {
+            let absPath = folder + fileSeparator + curr;
+            if (fs.statSync(absPath).isDirectory()) {
+                return prev.concat(getFileList(fs, absPath, fileList, fileSeparator));
+            }
+            else {
+                return prev.concat(absPath);
+            }
+        }, []);
+    }
+    catch (e) {
+        console.log(e);
+        return;
+    }
+
+};
+
+const getTargetFile = (fielList, regexFilter) => {
+    try {
+        let targetFileList = new Array();
+
+        fielList.map(file => {
+            if (!regexFilter.test(file)) { return; }
+            targetFileList = targetFileList.concat(file);
+        });
+
+        return targetFileList;
+    }
+    catch (e) {
+        console.log(e);
+        return;
+    }
+
+};
+
+const removeTag = (filePath, tagName, fs, cheeiro) => {
+
+    //If you dont want callback then synchronize
+    try {
+        let fileData = fs.readFileSync(filePath);
+        let $ = cheeiro.load(fileData.toString());
+
+        $(tagName).remove();
+
+        if (fs.writeFileSync(filePath, $.html().toString())) {
+            console.log("Write Failed");
+        }
+    }
+    catch (e) {
+        console.log(e);
+        return;
+    }
+};
+
+const main = () => {
+
+    const rootPath = process.argv.slice(2).toString();
+
+    let htmlFiles = getTargetFile(getFileList(FS, rootPath, new Array(), PATH.sep), /\.html/g);
+
+
+    htmlFiles.map(item => {
+        removeTag(item, "script", FS, CHEEIRO);
+    });
+};
+main();
